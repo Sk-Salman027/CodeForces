711a145753a63485e73b0108efdbbb6fe9cdedf9
Sk-Salman027 <graphicdsalman@gmail.com>
2025-04-14 03:30:09 +0600

    domino piling

---
+++ 50A-domino_piling.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    // variable declaration
+    int M, N;
+    // take input
+    scanf("%d %d", &M, &N);
+    // print the result
+    printf("%d\n", (M * N) / 2);
+
+    return 0;
+}
