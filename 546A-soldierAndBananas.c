f22439ae7d17fe6576bd152c0ef5c56fdab1e056
Sk-Salman027 <graphicdsalman@gmail.com>
2025-04-18 17:50:53 +0600

    soldier and bananas

---
+++ 546A-soldierAndBananas.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    // variable declaration
+    int initialDollars, totalDollars, numberOfBananas, borrow, cost = 0;
+    // take input
+    scanf("%d %d %d", &initialDollars, &totalDollars, &numberOfBananas);
+    for (int i = 1; i <= numberOfBananas; i++)
+    {
+        cost += initialDollars * i;
+    }
+    borrow = cost - totalDollars;
+    if (borrow > 0)
+    {
+        printf("%d\n", borrow);
+    }
+    else
+    {
+        printf("0\n");
+    }
+}
